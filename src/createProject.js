db846414f5e542df948e29beaeaaf1f196abe5ea
norms2098 <nrmsdgdgn@gmail.com>
2023-03-30 21:24:40 +0800

    Added a working addProject function

--- src/createProject.js
+++ src/createProject.js
@@ -2,9 +2,12 @@
 const addProject = () => {
     const createProjBtn = document.querySelector('.proj_btn');
     const removeForm = document.querySelector(".close_btn");
+    const addProjForm = document.querySelector(".addproj_form");
+    const projContainer = document.querySelector(".projects-menubar");
+    const buttonAddProj = document.querySelector(".proj_btn");
+    
 
     createProjBtn.addEventListener("click", ()=>{
-        console.log("hello")
         document.querySelector(".popup_div").classList.add("active");
         document.querySelector(".bg_popup").classList.add("active");
     
@@ -13,8 +16,20 @@ const addProject = () => {
         document.querySelector(".popup_div").classList.remove("active");
         document.querySelector(".bg_popup").classList.remove("active")
     })
+    addProjForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const ProjName = addProjForm.elements['project_name'].value;
+        const newProject = document.createElement("button");
+        newProject.innerHTML = ProjName;
+        newProject.className = ProjName;
 
+        projContainer.insertBefore(newProject,buttonAddProj);
+        addProjForm.reset();
+        document.querySelector(".popup_div").classList.remove("active");
+        document.querySelector(".bg_popup").classList.remove("active")
+    })
     
-}
-
+ }
+ 
 export default addProject;
+
